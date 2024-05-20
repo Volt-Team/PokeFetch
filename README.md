985344726cf311e5ca31eb3423d5c32dcf39ff77
meo209 <redslyice@outlook.de>
2024-05-20 19:20:07 +0200

    feat: fetch.py, fully functional fetcher.

---
+++ README.md
@@ -0,0 +1,15 @@
+# PokeFetch
+The image fetcher behind the [Volt](https://github.com/Volt-Team/Volt) Pokémon GO bot.
+
+# WARNING
+This effectively DDOS'es the bulbgarden website. BE WARNED.
+
+## Usage
+
+| Variable             | Description                                                             | Default    |
+|----------------------|-------------------------------------------------------------------------|------------|
+| images_per_pokemon   | Defines how many images per pokemon should be downloaded.               | 7          |
+| image_directory_name | Defines the name of the directory the pokemons should be downloaded to. | image_data |
+
+## License
+This project is licensed under the [GPLv3](https://www.gnu.org/licenses/gpl-3.0.html).
---
+++ fetch.py
@@ -0,0 +1,75 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# Base URL for the Pokémon page on Bulbapedia
+baseURL = "https://archives.bulbagarden.net/wiki/"
+
+# Fetching the list of Pokémon from the PokeAPI
+pokeapi_url = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+pokeapi_response = requests.get(pokeapi_url)
+pokeapi_data = pokeapi_response.json()
+
+images_per_pokemon = 7
+
+image_directory_name = "image_data"
+
+
+def fetch():
+    # Check if the request was successful
+    if pokeapi_response.status_code == 200:
+        # Extracting the names of the Pokémon
+        names = [pokemon['name'] for pokemon in pokeapi_data['results']]
+
+        # Ensure the 'image_directory_name' directory exists
+        if not os.path.exists(image_directory_name):
+            os.makedirs(image_directory_name)
+
+        for pokemonName in names:
+            url = baseURL + "Category:" + pokemonName.replace(' ', '_')
+            print(url)
+
+            response = requests.get(url=url)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+
+                img_tags = soup.find_all("li", {"class": "gallerybox"})
+
+                image_count = 0  # Initialize the counter for images per Pokémon
+
+                for tag in img_tags:
+                    width_tag = tag.find("div", style=lambda value: value and "width: 155px" in value)
+
+                    thumb_tag = width_tag.find("div", {"class": "thumb"})
+
+                    margin_tag = thumb_tag.find("div", style=lambda value: value and "margin:" in value)
+
+                    image = margin_tag.find("img", {"alt": ""})
+
+                    src = image["src"]
+
+                    # Construct the path where the image will be saved
+                    pokemon_dir = os.path.join(image_directory_name, pokemonName.lower().replace(' ', '_'))
+                    os.makedirs(pokemon_dir, exist_ok=True)  # Create the directory if it doesn't exist
+                    image_path = os.path.join(pokemon_dir, f"{image_count}.jpg")  # Assuming.jpg extension
+
+                    # Download and save the image
+                    image_response = requests.get(src)
+                    with open(image_path, 'wb') as file:
+                        file.write(image_response.content)
+
+                    print(f"Image saved to {image_path}")
+
+                    image_count += 1  # Increment the counter for each image fetched
+
+                    if image_count >= images_per_pokemon:  # Stop fetching images once 7 have been fetched
+                        break
+            else:
+                print(f"Failed to open {url}")
+    else:
+        print(f"Failed to open {pokeapi_url}")
+
+
+if __name__ == "__main__":
+    fetch()
